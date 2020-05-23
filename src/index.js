f07cb0fc43cc44f62a7fc78c748554d49147c878
akshaypatil581 <akshaypatil581@gmail.com>
2020-05-23 11:37:38 -0700

    Selected only needed functionalities from libraries explored different options for shortening the code

--- src/index.js
+++ src/index.js
@@ -1,33 +1,57 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React, {Component} from 'react'
+import {render} from 'react-dom'
 
+// So it's like having a huge data amd then what we do is we 
+/*
+  We are storing it in js and then assigning it to the mes/props in 
+  the component where we want it to be added 
+  then we simply assign it into its component
+*/
+
+let skiData = {
+  total: 50,
+  powder: 20,
+  backcountry: 10,
+  goal: 100
+}
 
 // Creating first component
 // all of the component names should start with capital
-class Message extends React.Component {
-  // All react component have render method
-  /*
-  render method discribes what we want to render into dom
-  */
+class SkiDayCounter extends Component {
+  getPercent = (decimal) => {
+    return decimal * 100 + '%'
+  }
+  calGoalProgress = (total, goal) => {
+    return this.getPercent(total/goal)
+  }
+
   render() {
-    console.log(this.props);
-    
+    const {total, powder, backcountry, goal} = this.props;
     return (
-      <div>
-        <h1 style={{color: this.props.color}}>{this.props.msg}</h1>
-        <p>I'll check back in {this.props.minutes} minutes</p>
-      </div>
+      <section>
+        <div>
+          <p>Total Days: {total}</p>
+        </div>
+        <div>
+          <p>Powder Days: {powder}</p>
+        </div>
+        <div>
+          <p>Backcountry Days: {backcountry}</p>
+        </div>
+        <div>
+          <p>Goal Progress: {this.calGoalProgress(total, goal)}</p>
+        </div>
+      </section>
     )
   }
 }
-
 // Right now everthing is static what if we want to do this dynamic 
 // for this we will be using props, first step to work with props is to pass the data to
 // message component
 
 // for message if we are passing anyother thing than string then we have to wrap it around {} brackets
-ReactDOM.render(
-  <Message minutes={20} color="blue" msg="How are you doing?"/>,
+render(
+  <SkiDayCounter total={skiData.total} powder={skiData.powder} backcountry={skiData.backcountry} goal={skiData.goal} />,
   document.getElementById('root')
 )
 
