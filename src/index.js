c0630cd10b9a9f1d89db88b9d661c7bda099dca3
akshaypatil581 <akshaypatil581@gmail.com>
2020-05-23 14:09:44 -0700

    Completed composing components

--- src/index.js
+++ src/index.js
@@ -1,125 +1,27 @@
 import React, {Component} from 'react'
 import {render} from 'react-dom'
 
-// So it's like having a huge data amd then what we do is we 
-/*
-  We are storing it in js and then assigning it to the mes/props in 
-  the component where we want it to be added 
-  then we simply assign it into its component
-*/
-
-let skiData = {
-  total: 50,
-  powder: 20,
-  backcountry: 10,
-  goal: 100
-}
-
-// Component using function
-// here notice that we don't have access to this because this is not a 
-// class but we have acces to props which we can use without using this
-const SkiDayCounter = (props) => {
+const Book = ({title, auther, pages}) => {
   return (
     <section>
-      <div>
-        <p>Total Days: {props.total}</p>
-      </div>
-      <div>
-        <p>Powder Days: {props.powder}</p>
-      </div>
-      <div>
-        <p>Backcountry Days: {props.backcountry}</p>
-      </div>
-      <div>
-        <p>Goal Progress: {props.goal}</p>
-      </div>
+      <h2>{title}</h2>
+      <p>by: {auther}</p>
+      <p>Pages: {pages} pages</p>
     </section>
   )
 }
 
-// Creating first component
-// all of the component names should start with capital
-// class SkiDayCounter extends Component {
-//   getPercent = (decimal) => {
-//     return decimal * 100 + '%'
-//   }
-//   calGoalProgress = (total, goal) => {
-//     return this.getPercent(total/goal)
-//   }
+const Library = () => {
+  return (
+    <div>
+      <Book title="The Sun Also Rises" auther="Ernest Hemingway" pages={260}/>
+      <Book title="White Teeth" auther="Zadie Smith" pages={480}/>
+      <Book title="Cat's Cradle" auther="Kurt Vonnegut" pages={304}/>
+    </div>
+  )
+}
 
-//   render() {
-//     const {total, powder, backcountry, goal} = this.props;
-//     return (
-//       <section>
-//         <div>
-//           <p>Total Days: {total}</p>
-//         </div>
-//         <div>
-//           <p>Powder Days: {powder}</p>
-//         </div>
-//         <div>
-//           <p>Backcountry Days: {backcountry}</p>
-//         </div>
-//         <div>
-//           <p>Goal Progress: {this.calGoalProgress(total, goal)}</p>
-//         </div>
-//       </section>
-//     )
-//   }
-// }
-// Right now everthing is static what if we want to do this dynamic 
-// for this we will be using props, first step to work with props is to pass the data to
-// message component
-
-// for message if we are passing anyother thing than string then we have to wrap it around {} brackets
-render(
-  <SkiDayCounter total={skiData.total} powder={skiData.powder} backcountry={skiData.backcountry} goal={skiData.goal} />,
+render (
+  <Library />,
   document.getElementById('root')
 )
-
-/*
-So far we created react components as ES6 classes but that's not the only way to create 
-classes we can create class/components using JS function as well
-*/
-
-/*
-Reason react is so popular is because of it's
-component based structure 
-
-We simply create elements by creating nested components and we
-render these components 
-
-*/  
-
-/*
-Now we are going to specify styles which we want to apply to elements
-*/
-/*
-  create element accepts 3 arguments
-  1. element we want to create 
-  2. attribute for the elements 
-  3. content for the element
-*/
-
-/*
-  To simplyfy some creation thins React provides us the format of JSX
-  JavaScript as XML, tag based syntax to create react elements 
-*/
-
-// const title = React.createElement(
-//   'ul',
-//   {id: 'title', className: 'header', style: style},
-//   React.createElement(
-//     'li',
-//     {},
-//     'item on our list'
-//   )
-// )
-
-/* 
-  ReactDOM Accepts
-  1. what you wnat to render 
-  2. where you want to render 
-*/
-//This is JSX not HTML
-
