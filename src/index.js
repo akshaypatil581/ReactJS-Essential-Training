f241d4f2f87ff915456943ec556a80c23ac6029b
akshaypatil581 <akshaypatil581@gmail.com>
2020-05-23 10:25:48 -0700

    Started reactJS tutorial

--- src/index.js
+++ src/index.js
@@ -1,17 +1,58 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
+/*
+Now we are going to specify styles which we want to apply to elements
+*/
+
+var style = {
+  backgroundColor: 'orange',
+  color: 'white',
+  fontFamily: 'Arial'
+}
+
+/*
+  create element accepts 3 arguments
+  1. element we want to create 
+  2. attribute for the elements 
+  3. content for the element
+*/
+
+/*
+  To simplyfy some creation thins React provides us the format of JSX
+  JavaScript as XML, tag based syntax to create react elements 
+*/
+
+// const title = React.createElement(
+//   'ul',
+//   {id: 'title', className: 'header', style: style},
+//   React.createElement(
+//     'li',
+//     {},
+//     'item on our list'
+//   )
+// )
+
+/* 
+  ReactDOM Accepts
+  1. what you wnat to render 
+  2. where you want to render 
+*/
+//This is JSX not HTML
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <div style={style}>
+    <h1 id="heading-element">Hello World!</h1>
+    <p>I'm glad I'm here</p>
+  </div>,
   document.getElementById('root')
-);
+)
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+/*
+Reason react is so popular is because of it's
+component based structure 
+
+We simply create elements by creating nested components and we
+render these components 
+
+*/  
+
